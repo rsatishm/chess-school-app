b654c3057b810d05b8b0401800757e38fb958f52
rsatish.m@gmail.com <ext.rama.satish.mokkapaty@navis.com>
2022-05-18 09:16:07 +0530

    classroom - jitsi integration

--- src/pages/coach-app/classtooms/create-classroom-form.tsx
+++ src/pages/coach-app/classtooms/create-classroom-form.tsx
@@ -1,3 +1,7 @@
+import { Jitsi } from "../../../components/jitsi/Jitsi"
+
 export const CreateClassRoom = ()=>{
-    return <div>Create classroom</div>
+    return <div>Create classroom
+            <Jitsi/>
+             </div>
 }
