51d1b14e513da7f249140cc5194befe6c6bb164e
Ram Satish <ext.rama.satish.mokkapaty@navis.com>
2022-06-29 20:22:49 +0530

    pgn

--- src/pages/coach-app/problembase/my-problembases/my-database.tsx
+++ src/pages/coach-app/problembase/my-problembases/my-database.tsx
@@ -8,7 +8,7 @@ import '../../../common-pages/analysis-board/analysis-board.less'
 import '../../../../components/scoresheet/scoresheet.less'
 import { Link, useNavigate } from 'react-router-dom'
 import Dragger from 'antd/lib/upload/Dragger'
-import { BackwardOutlined, FastBackwardOutlined, FastForwardOutlined, ForwardOutlined, InboxOutlined, SwapOutlined } from '@ant-design/icons'
+import { BackwardOutlined, FastBackwardOutlined, FastForwardOutlined, ForwardOutlined, InboxOutlined, SwapOutlined, UploadOutlined } from '@ant-design/icons'
 import SplitterLayout from 'react-splitter-layout';
 import 'react-splitter-layout/lib/index.css';
 import { DataNode, DirectoryTreeProps } from 'antd/lib/tree'
@@ -17,29 +17,49 @@ import { Chessboard } from '../../../../components/chessboard/Chessboard'
 import * as _ChessJS from 'chess.js';
 import { SetupChessboard } from '../../../../components/chessboard/setup-chessboard'
 import Chessgroundboard from '../../../../components/chessgroundboard/Chessgroundboard'
+import { PgnUploadModal } from './pgn-upload-modal'
 
 interface State {
-  showUploadPgn: boolean
+  uploadPgnVisible: boolean,
+  fileList: Array<any>
 }
+const ONE_KILO_BYTE = 1024
+// const TWO_FIFTY_KBYTES = 250 * ONE_KILO_BYTE
+const TWO_THOUSAND_KBYTES = 2000 * ONE_KILO_BYTE
 
 export const MyDatabase = observer(() => {
-  const [state, setState] = useState({
-    showUploadPgn: false
+  const { gameboxDatabaseStore, gameboxDatabaseGameStore } = React.useContext(MobXProviderContext)
+  const [state, setState] = useState<State>({
+    uploadPgnVisible: false,
+    fileList: []
   })
+
   const updateState = (newState: Partial<State>) => {
     setState((prevState) => {
-      return { ...prevState, ...newState }
+        return { ...prevState, ...newState }
     })
   }
   const handleOk = () => {
     updateState({
-      showUploadPgn: false
+      uploadPgnVisible: false
     })
   }
+
   const handleUploadPGN = () => {
     updateState({
-      showUploadPgn: true
+      uploadPgnVisible: true
     })
+  }
+
+  const toggleUploadPgnVisible = () => {
+    updateState({
+      uploadPgnVisible: !state.uploadPgnVisible
+    })
+  }
+
+  const uploadPgn = (values: any) => {
+      const data = values.pgnFile.file.originFileObj
+      console.log("Data: " + data)
   }
 
   const props: UploadProps = {
@@ -59,11 +79,42 @@ export const MyDatabase = observer(() => {
     },
     onDrop(e) {
       console.log('Dropped files', e.dataTransfer.files);
-    },
+    }
   };
 
+  function isPGN(file: File) {
+    return file.name.endsWith('.pgn')
+  }
+  
+  function isLessThan2MB(file: File) {
+    return file.size < TWO_THOUSAND_KBYTES
+  }
+  
+
+  const beforeUpload = (file: File) => {
+    if (!isPGN(file)) {
+      message.error('Only PGN files are allowed')
+    }
+    if (!isLessThan2MB(file)) {
+      message.error('PGN file size must be lesser than 2 MB')
+    }
+
+    if (isPGN(file) && isLessThan2MB(file)) {
+      const reader: FileReader = new FileReader();
+      updateState({fileList: [{ uid: 1, name: file.name, status: 'done', url: reader.result }]})
+    }
+
+    return false
+  }
+
   const UploadPgn: React.FC = () => (
-    <Dragger {...props}>
+    <Dragger 
+    disabled={gameboxDatabaseStore!.uploading}
+    accept=".pgn,application/x-chess-pgn"
+    supportServerRender={true}
+    showUploadList={false}
+    fileList={state.fileList ? state.fileList : []}
+    beforeUpload={beforeUpload}>
       <p className="ant-upload-drag-icon">
         <InboxOutlined />
       </p>
@@ -93,16 +144,16 @@ export const MyDatabase = observer(() => {
       key: '0-1'
     },
   ];
-  
+
   const DatabaseTree: React.FC = () => {
     const onSelect: DirectoryTreeProps['onSelect'] = (keys, info) => {
       console.log('Trigger Select', keys, info);
     };
-  
+
     const onExpand: DirectoryTreeProps['onExpand'] = (keys, info) => {
       console.log('Trigger Expand', keys, info);
     };
-  
+
     return (
       <DirectoryTree
         multiple
@@ -117,81 +168,113 @@ export const MyDatabase = observer(() => {
   const ChessJS = typeof _ChessJS === 'function' ? _ChessJS : _ChessJS.Chess
   const fen = new ChessJS().fen()
 
-  const backward = ()=>{}
-  const prev=()=>{}
-  const handleFlip = ()=>{}
-  const next = ()=>{}
-  const forward = ()=>{}
+  const backward = () => { }
+  const prev = () => { }
+  const handleFlip = () => { }
+  const next = () => { }
+  const forward = () => { }
 
-  const ChessboardPosition = ()=>{
+  const PgnUploadLocal = ()=>{
+    return <Modal
+        title={
+          gameboxDatabaseStore!.uploading
+            ? 'Uploading PGN...'
+            : 'Upload PGN'
+        }
+        style={{ width: 600 }}
+          visible={state.uploadPgnVisible}
+          width={800}
+          maskClosable={false}
+          onCancel={handleOk}
+          onOk={handleOk} >
+          <div className="position-setup-modal" title="Setup Position">
+            <Form name="pgn-upload" onFinish={uploadPgn}>
+              <Form.Item name="pgnFile" rules={[{ required: true, message: 'Upload PGN file' }]} label="PGN File">
+                <UploadPgn />
+              </Form.Item>
+              <Button type='primary' block onClick={handleUploadPGN}>
+                Create Database
+              </Button>
+              <Button type='primary' block onClick={handleUploadPGN}>
+                Merging with Database
+              </Button>
+              <Form.Item name="name" rules={[{ required: true, message: 'Name is required' }]} label="Database name">
+                <Input placeholder="Enter New Database Name" />
+              </Form.Item>
+            </Form>
+          </div>
+        </Modal>
+  } 
+
+  const ChessboardPosition = () => {
     return (<Row className="analysis-board scoresheet-container">
-    <Col md={{ span: 12, offset: 2 }} sm={24}>
-      <Chessgroundboard
-        height={600}
-        width={600}
-        orientation='w'
-        fen={fen}
-        turnColor='white'
-        onMove={()=>{}}
-      />
-      <Row            
-        justify="center"
-        style={{ marginTop: '1rem', marginBottom: '1rem' }}
-      >
-        <Col span={2} offset={1}>
-          <Tooltip title="fast-backward (< key)">
-            <Button
-              icon={<FastBackwardOutlined/>}
-              type="ghost"
-              shape="circle"
-              onClick={backward}
-            />
-          </Tooltip>
-        </Col>
-        <Col span={2}>
-          <Tooltip title="backward (left arrow)">
-            <Button
-              icon={<BackwardOutlined/>}
-              type="ghost"
-              shape="circle"
-              onClick={prev}
-            />
-          </Tooltip>
-        </Col>
-        <Col span={2}>
-          <Tooltip title="flip board (f key)">
-            <Button
-              icon={<SwapOutlined/>}
-              style={{ transform: 'rotate(90deg)' }}
-              type="ghost"
-              shape="circle"
-              onClick={handleFlip} //{this.props.analysisBoardStore!.prev} //change this
-            />
-          </Tooltip>
-        </Col>
-        <Col span={2}>
-          <Tooltip title="forward (right arrow)">
-            <Button
-              icon={<ForwardOutlined/>}
-              type="ghost"
-              shape="circle"
-              onClick={next}
-            />
-          </Tooltip>
-        </Col>
-        <Col span={2}>
-          <Tooltip title="fast-forward (> key)">
-            <Button
-              icon={<FastForwardOutlined/>}
-              type="ghost"
-              shape="circle"
-              onClick={forward}
-            />
-          </Tooltip>
-        </Col>
-      </Row>
-    </Col>
-  </Row>)
+      <Col md={{ span: 12, offset: 2 }} sm={24}>
+        <Chessgroundboard
+          height={600}
+          width={600}
+          orientation='w'
+          fen={fen}
+          turnColor='white'
+          onMove={() => { }}
+        />
+        <Row
+          justify="center"
+          style={{ marginTop: '1rem', marginBottom: '1rem' }}
+        >
+          <Col span={2} offset={1}>
+            <Tooltip title="fast-backward (< key)">
+              <Button
+                icon={<FastBackwardOutlined />}
+                type="ghost"
+                shape="circle"
+                onClick={backward}
+              />
+            </Tooltip>
+          </Col>
+          <Col span={2}>
+            <Tooltip title="backward (left arrow)">
+              <Button
+                icon={<BackwardOutlined />}
+                type="ghost"
+                shape="circle"
+                onClick={prev}
+              />
+            </Tooltip>
+          </Col>
+          <Col span={2}>
+            <Tooltip title="flip board (f key)">
+              <Button
+                icon={<SwapOutlined />}
+                style={{ transform: 'rotate(90deg)' }}
+                type="ghost"
+                shape="circle"
+                onClick={handleFlip} //{this.props.analysisBoardStore!.prev} //change this
+              />
+            </Tooltip>
+          </Col>
+          <Col span={2}>
+            <Tooltip title="forward (right arrow)">
+              <Button
+                icon={<ForwardOutlined />}
+                type="ghost"
+                shape="circle"
+                onClick={next}
+              />
+            </Tooltip>
+          </Col>
+          <Col span={2}>
+            <Tooltip title="fast-forward (> key)">
+              <Button
+                icon={<FastForwardOutlined />}
+                type="ghost"
+                shape="circle"
+                onClick={forward}
+              />
+            </Tooltip>
+          </Col>
+        </Row>
+      </Col>
+    </Row>)
   }
 
   return (
@@ -199,41 +282,32 @@ export const MyDatabase = observer(() => {
       <div className='header'>
         <p className="title"><span className='cursor-pointer'>Database</span></p>
         <div className='button'>
-          <Button type='primary' block onClick={handleUploadPGN}>
+          <Button type='primary' block onClick={toggleUploadPgnVisible} loading={gameboxDatabaseStore!.uploading}>
+          <UploadOutlined
+                  style={{
+                    opacity: gameboxDatabaseStore!.uploading
+                      ? 0
+                      : 1
+                  }}
+                />{' '}
             Upload PGN
           </Button>
+
         </div>
       </div>
-      {state.showUploadPgn && (
-        <Modal
-          title="Upload PGN"
-          visible={state.showUploadPgn}
-          style={{ top: 25 }}
-          width={800}
-          maskClosable={false}
-          onCancel={handleOk}
-          onOk={handleOk} >
-          <div className="position-setup-modal" title="Setup Position">
-            <UploadPgn />
-            <Button type='primary' block onClick={handleUploadPGN}>
-              Create Database
-            </Button>
-            <Button type='primary' block onClick={handleUploadPGN}>
-              Merging with Database
-            </Button>
-            <Form.Item name="name" rules={[{ required: true, message: 'Name is required' }]} label="Database name">
-              <Input placeholder="Enter New Database Name" />
-            </Form.Item>
-          </div>
-        </Modal>
+      {state.uploadPgnVisible && (
+        <PgnUploadModal
+        visible={state.uploadPgnVisible}
+        onClose={toggleUploadPgnVisible}
+      />
       )}
       <div className='inner'>
         <SplitterLayout percentage secondaryInitialSize={85}>
-          <DatabaseTree/>
+          <DatabaseTree />
           <SplitterLayout percentage secondaryInitialSize={65}>
             <div>PNG details</div>
             <div>
-            <ChessboardPosition/>
+              <ChessboardPosition />
             </div>
           </SplitterLayout>
         </SplitterLayout>
---
+++ src/pages/coach-app/problembase/my-problembases/pgn-upload-modal.tsx
@@ -0,0 +1,262 @@
+import { ChangeEvent, useContext, useState } from 'react'
+import { MobXProviderContext, observer } from 'mobx-react'
+import {
+  Modal,
+  Upload,
+  Divider,
+  Input,
+  Radio,
+  Select,
+  Button,
+  message,
+  Progress
+} from 'antd'
+import { RadioChangeEvent } from 'antd/lib/radio'
+
+import './pgn-upload-modal.less'
+
+import { CloseCircleOutlined, UploadOutlined } from '@ant-design/icons'
+
+const ONE_KILO_BYTE = 1024
+// const TWO_FIFTY_KBYTES = 250 * ONE_KILO_BYTE
+const TWO_THOUSAND_KBYTES = 2000 * ONE_KILO_BYTE
+
+interface Props {
+  visible: boolean
+  onClose: () => any
+}
+
+interface State {
+  afterUploadAction: 'create' | 'merge'
+  file: null | File
+  newDatabaseName: string
+  mergeDatabaseUuid?: string
+}
+
+function isPGN(file: File) {
+  return file.name.endsWith('.pgn')
+}
+
+function isLessThan2MB(file: File) {
+  return file.size < TWO_THOUSAND_KBYTES
+}
+
+const INIT_STATE: State = {
+  afterUploadAction: 'create', // or 'merge'
+  file: null,
+  newDatabaseName: '',
+  mergeDatabaseUuid: undefined
+}
+
+export const PgnUploadModal = observer((props: Props) => {
+  const { gameboxDatabaseStore } = useContext(MobXProviderContext)
+  const [state, setState] = useState<State>(INIT_STATE)
+
+  const updateState = (newState: Partial<State>) => {
+    setState((prevState) => {
+      return { ...prevState, ...newState }
+    })
+  }
+
+  const cleanUpAndClose = () => {
+    props.onClose()
+    updateState(INIT_STATE)
+  }
+
+  const allFieldsValid = () => {
+    return (
+      (!!state.file &&
+        state.afterUploadAction === 'create' &&
+        state.newDatabaseName.trim()) ||
+      (state.afterUploadAction === 'merge' && state.mergeDatabaseUuid)
+    )
+  }
+
+  const databaseSelectFilterOption = (inputValue: string, option: any) => {
+    return (
+      option.props.children
+        .toString()
+        .toLowerCase()
+        .indexOf(inputValue.toLowerCase()) >= 0
+    )
+  }
+
+  const handleNewDatabaseNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    updateState({ newDatabaseName: e.currentTarget.value })
+  }
+
+  const handleMergeDatabaseChange = (uuid: any) => {
+    updateState({ mergeDatabaseUuid: uuid })
+  }
+
+  const handleAfterUploadActionChange = (action: RadioChangeEvent) => {
+    updateState({
+      afterUploadAction: action.target.value as 'create' | 'merge'
+    })
+  }
+
+  const handleFileRemove = () => {
+    updateState({ file: null })
+  }
+
+  const beforeUpload = (file: File) => {
+    if (!isPGN(file)) {
+      message.error('Only PGN files are allowed')
+    }
+    if (!isLessThan2MB(file)) {
+      message.error('PGN file size must be lesser than 2 MB')
+    }
+
+    if (isPGN(file) && isLessThan2MB(file)) {
+      updateState({ file })
+    }
+
+    return false
+  }
+
+  const handleUpload = async () => {
+    try {
+      await gameboxDatabaseStore!.upload({
+        file: state.file!,
+        ...(state.afterUploadAction === 'create'
+          ? { create: state.newDatabaseName }
+          : {}),
+        ...(state.afterUploadAction === 'merge'
+          ? { merge: state.mergeDatabaseUuid }
+          : {})
+      })
+      cleanUpAndClose()
+      message.success('Uploaded, parsing PGN')
+    } catch (e) {
+      console.log('--> error: ', e)
+      message.error('There was an error uploading the PGN')
+    }
+  }
+
+  const renderUploader = () => {
+    if (state.file) {
+      return (
+        <div className="ant-upload ant-upload-drag">
+          {gameboxDatabaseStore!.uploading && (
+            <Progress
+              style={{ position: 'relative', top: -8 }}
+              percent={gameboxDatabaseStore!.uploadProgressPercent}
+              status="active"
+              showInfo={false}
+            />
+          )}
+          <span className="ant-upload ant-upload-btn">
+            <p className="ant-upload-drag-icon">
+              <UploadOutlined />
+            </p>
+            <p className="ant-upload-text">
+              {state.file.name}{' '}
+              <Button
+                disabled={gameboxDatabaseStore!.uploading}
+                size="small"
+                danger
+                shape="circle"
+                onClick={handleFileRemove}
+              >
+                <CloseCircleOutlined />
+              </Button>
+            </p>
+            <p className="ant-upload-hint">
+              {(state.file.size / ONE_KILO_BYTE).toFixed(2)}kB
+            </p>
+          </span>
+        </div>
+      )
+    }
+
+    return (
+      <Upload.Dragger
+        disabled={gameboxDatabaseStore!.uploading}
+        accept=".pgn,application/x-chess-pgn"
+        supportServerRender={true}
+        showUploadList={false}
+        fileList={state.file ? [state.file] : []}
+        beforeUpload={beforeUpload}
+      >
+        <p className="ant-upload-drag-icon">
+          <UploadOutlined />
+        </p>
+        <p className="ant-upload-text">Click or drag PGN File to this area</p>
+        <p className="ant-upload-hint">File size limit: 2 MB</p>
+      </Upload.Dragger>
+    )
+  }
+
+  const mockOptions = gameboxDatabaseStore!.databases.map(
+    (d: any) => (
+      <Select.Option key={d.uuid} value={d.uuid}>
+        {d.name}
+      </Select.Option>
+    )
+  )
+  return (
+    <Modal
+      title={
+        gameboxDatabaseStore!.uploading
+          ? 'Uploading PGN...'
+          : 'Upload PGN'
+      }
+      style={{ width: 600 }}
+      visible={props.visible}
+      onCancel={cleanUpAndClose}
+      maskClosable={false}
+      okButtonProps={{
+        loading: gameboxDatabaseStore!.uploading,
+        disabled: !allFieldsValid()
+      }}
+      okText="Upload"
+      cancelButtonProps={{
+        disabled: gameboxDatabaseStore!.uploading
+      }}
+      closable={!gameboxDatabaseStore!.uploading}
+      destroyOnClose={true}
+      onOk={handleUpload}
+    >
+      {renderUploader()}
+      <Divider>
+        After upload: &nbsp;
+        <Radio.Group
+          disabled={gameboxDatabaseStore!.uploading}
+          defaultValue="create"
+          buttonStyle="solid"
+          onChange={handleAfterUploadActionChange}
+          value={state.afterUploadAction}
+        >
+          <Radio.Button value="create">Create Database</Radio.Button>
+          <Radio.Button value="merge">Merge with Existing</Radio.Button>
+        </Radio.Group>
+      </Divider>
+      {state.afterUploadAction === 'create' && (
+        <Input
+          disabled={gameboxDatabaseStore!.uploading}
+          placeholder="Enter New Database Name"
+          onChange={handleNewDatabaseNameChange}
+          value={state.newDatabaseName}
+          maxLength={50}
+        />
+      )}
+      {state.afterUploadAction === 'merge' && (
+        <Select
+          disabled={gameboxDatabaseStore!.uploading}
+          style={{ width: '100%' }}
+          showSearch={true}
+          placeholder="Search for an existing database"
+          defaultActiveFirstOption={false}
+          showArrow={false}
+          filterOption={databaseSelectFilterOption}
+          // onSearch={this.handleSearch}
+          onChange={handleMergeDatabaseChange}
+          value={state.mergeDatabaseUuid}
+        // notFoundContent={null}
+        >
+          {mockOptions}
+        </Select>
+      )}
+    </Modal>
+  )
+})
