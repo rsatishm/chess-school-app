a585bc74dc3f823b2b94aae557acc485d55efc67
Ram Satish <ext.rama.satish.mokkapaty@navis.com>
2022-07-13 22:02:17 +0530

    assignment

--- src/pages/coach-app/problembase/my-problembases/chessboard-list.tsx
+++ src/pages/coach-app/problembase/my-problembases/chessboard-list.tsx
@@ -1,12 +1,33 @@
-import { List } from "antd"
+import { Checkbox, List } from "antd"
 import { MobXProviderContext } from "mobx-react"
 import { observer } from "mobx-react-lite"
-import React from "react"
-import InfiniteScroll from "react-infinite-scroller"
+import React, { useState } from "react"
 import { ChessboardPosition } from "./chessboard-position."
 
 export const ChessboardList = observer(() => {
-  const { gameboxDatabaseStore } = React.useContext(MobXProviderContext)
+    interface State {
+        pgnSelected: any
+    }
+    const [state, setState] = useState<State>({
+        pgnSelected: {}
+    })
+    const { gameboxDatabaseStore } = React.useContext(MobXProviderContext)
+
+    const handlePgnSelected = (e: any) => {
+        console.log(JSON.stringify(e))
+        console.log("State:" + JSON.stringify(state))
+        setState((prevState: State) => {
+            console.log("prevState: " + JSON.stringify(prevState))
+            let uuidChecked: State = { pgnSelected: {} }
+            uuidChecked.pgnSelected[e.target.value] = e.target.checked
+            const newState = {
+                pgnSelected: { ...prevState.pgnSelected, ...uuidChecked.pgnSelected }
+            }
+            console.log("newState: " + JSON.stringify(newState))
+            return newState
+        })
+    }
+
     const data: any[] = gameboxDatabaseStore!.loading ? [] :
         gameboxDatabaseStore!.pgnList.map((pgnRec: any) => ({
             sno: pgnRec.sno,
@@ -18,39 +39,34 @@ export const ChessboardList = observer(() => {
             fen: pgnRec.fen,
             pgnTitle: pgnRec.white + ' vs ' + pgnRec.black + ' ' + pgnRec.result
         }))
-    const list = 
 
-    <List
-    itemLayout="vertical"
-    style={ {overflow: 'auto', height: '500px'}}
-    pagination={{
-        onChange: page => {
-          console.log(page);
-        },
-        pageSize: 30,
-      }}
-    loading={gameboxDatabaseStore!.loading}
-      dataSource={data}
-      renderItem={item => (
-        <List.Item key={item.uuid}>
-          <List.Item.Meta/>
-          <div>
-          <ChessboardPosition
-                    pgn={item.pgn}
-                    pgnTitle={item.pgnTitle} />
-          </div>
-        </List.Item>
-      )}
+    const list = <List
+        itemLayout="vertical"
+        style={{ overflow: 'auto', height: '420px' }}
+        pagination={{
+            onChange: page => {
+                console.log(page);
+            },
+            pageSize: 30,
+        }}
+        loading={gameboxDatabaseStore!.loading}
+        dataSource={data}
+        renderItem={item => (
+            <List.Item key={item.uuid}>
+                <List.Item.Meta />
+                <div>
+                    <div className="left">
+                        <Checkbox
+                            value={item.uuid}
+                            checked={state.pgnSelected[item.uuid] ? state.pgnSelected[item.uuid] : false}
+                            onChange={handlePgnSelected}><strong>Selected</strong></Checkbox>
+                    </div>
+                    <ChessboardPosition
+                        pgn={item.pgn}
+                        pgnTitle={item.pgnTitle} />
+                </div>
+            </List.Item>
+        )}
     />
-    /*
-    return <div>
-        {
-            data.map((rec: any) => {
-                return <ChessboardPosition
-                    pgn={rec?.pgn}
-                    pgnTitle={rec?.pgnTitle} />
-            })
-        }
-    </div>*/
     return list
 })
